f4f4688fbf8437fa635daedad89f22c2838996f3
Rob Latour <5200730+roblatour@users.noreply.github.com>
2024-11-29 14:41:40 -0500

    remove duplicate folder

--- AViewerForWindows/AViewerForWindows/ObfuscationSettings.vb
+++
@@ -1,4 +0,0 @@
-﻿Imports System.Reflection
-
-<Assembly: Obfuscation(Feature:="encrypt symbol names with password 57UeWPxHUUljjjjjjjjjjjjjjjjdtdjjjjjjjjjjjjjjjjjj1D4lH0buEaTzZnD1", Exclude:=False)>
-<Assembly: Obfuscation(Feature:="code control flow obfuscation", Exclude:=False)>
